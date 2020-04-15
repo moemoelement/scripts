7ed929f44e2de61e82c5057eae6698ca72b080fc
moemoelement <63739912+moemoelement@users.noreply.github.com>
2020-04-15 18:48:55 +0200

    Add files via upload

---
+++ VMcreate.ps1
@@ -0,0 +1,19 @@
+﻿# variables utilisées plus tard dans le script
+param ($VMName, $MemorySize, $VHDSize)
+$InstallMedia = 'D:\ISO Windows Server\Windows Server 2016 Standard.ISO'
+
+# création de la VM
+New-VM -Name $VMName -MemoryStartupBytes $MemorySize -Generation 2 -NewVHDPath "C:\VM\$VMName\$VMName.vhdx" -NewVHDSizeBytes $VHDSize -Path "C:\VM\$VMName"
+
+# ajout du lecteur de DVD
+Add-VMScsiController -VMName $VMName
+Add-VMDvdDrive -VMName $VMName -ControllerNumber 1 -ControllerLocation 0 -Path $InstallMedia
+
+# montage de l'image iso Windows Server 2016 sur le lecteur DVD
+$DVDDrive = Get-VMDvdDrive -VMName $VMName
+
+# boot de la VM sur le lecteur DVD au démarrage
+Set-VMFirmware -VMName $VMName -FirstBootDevice $DVDDrive
+
+# connexion au service de visualisation avec la commande "VMConnect.exe"
+# le seul problème est qu'il faut appuyer sur une touche quelconque pour lancer le boot sur DVD lors du démarrage de la VM (moyen de s'en affranchir ?)
---
+++ VMdelete.ps1
@@ -0,0 +1,9 @@
+﻿param ($VMName)
+$confirmation = Read-Host "Are you sure you want to delete all files for $VMName ? (y/n)"
+if ($confirmation -eq 'y') {
+Remove-VMDvdDrive -VMName $VMName -ControllerNumber 1 -ControllerLocation 0
+Remove-VMHardDiskDrive -VMName $VMName -ControllerType SCSI -ControllerNumber 0 -ControllerLocation 0
+Remove-VM -Name $VMName -Force
+write-host "$VMName has been successfully deleted"
+} else {
+write-host "The action has been cancelled"}
